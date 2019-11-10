2f2d3f5ba66afe46d05d97e830225247f5be0be4
Stefan Matting <stefan.matting@gmail.com>
2019-11-10 23:28:18 +0100

    update lastpic.py

--- tools/lastpic.py
+++ tools/lastpic.py
@@ -1,19 +1,60 @@
-import sqlite3
 import sys
+import base64
+import sqlite3
+import socketio
+import time
 
-def get_last(filename):
+
+class Config:
+    def __init__(self, host_url, dbfilename):
+        self.host_url = host_url
+        self.dbfilename = dbfilename
+
+
+def read_file(filename):
+    with open(filename, "rb") as image_file:
+        base64_bytes = base64.b64encode(image_file.read())
+    return base64_bytes.decode('utf-8')
+
+
+def update_image(sio, filename):
+    print('emitting new image')
+    base64_string = read_file(filename)
+    sio.emit('action', {"type": 'LIVEIMAGE_PUSH', "liveImage": base64_string})
+
+
+def get_last(dbfilename):
     '''
     load frames out of a Motion sqlite3 database
     '''
-    with sqlite3.connect(filename) as conn:
+    with sqlite3.connect(dbfilename) as conn:
         sql = 'select filename from security order by time_stamp desc limit 1'
         cursor = conn.cursor()
         cursor.execute(sql)
         return cursor.fetchone()[0]
 
-def main(filename):
-    print(get_last(filename))
 
+def main(cfg):
+    sio = socketio.Client()
+    sio.connect(cfg.host_url)
+
+    filename = None
+    while True:
+        filename_poll = get_last(cfg.dbfilename)
+        if filename_poll != filename:
+            filename = filename_poll
+        if filename is not None:
+            filename(sio, filename)
+        time.sleep(0.1)
+
+
+usage = '''
+Usage: ./lastpic URL DBFILE
+'''
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    if len(sys.argv) != 3:
+        print(usage)
+        sys.exit(1)
+    cfg = Config(sys.argv[1], sys.argv[2])
+    main(cfg)
