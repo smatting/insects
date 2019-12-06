d118f56dbb5396849d9102773ac564b716db8714
Stefan Matting <stefan.matting@gmail.com>
2019-12-07 00:23:05 +0100

    Add custom nix stuff

--- backend/shell.nix
+++ backend/shell.nix
@@ -1,8 +1,17 @@
-{ pkgs ? (import <nixpkgs> {}) }:
-with pkgs;
-(python37.withPackages(ps: with ps; [
-  flask
-  flask-socketio
-  ipython
-  ipdb
-])).env
+with import <nixpkgs> {};
+
+(let
+  python = let
+    packageOverrides = self: super: {
+      promise = self.callPackage ./nix/promise/default.nix {};
+      aniso8601 = self.callPackage ./nix/aniso8601/default.nix {};
+      graphql-core = self.callPackage ./nix/graphql-core/default.nix {};
+      graphql-relay = self.callPackage ./nix/graphql-relay/default.nix {};
+      graphene-sqlalchemy = self.callPackage ./nix/graphene-sqlalchemy/default.nix {};
+      python-graphene = self.callPackage ./nix/graphene/default.nix {};
+    };
+  in pkgs.python37.override {inherit packageOverrides; self = python;};
+
+in python.withPackages(ps: with ps; [ipython ipdb python-graphene flask flask-socketio graphene-sqlalchemy])).env
+
+  
