c4be51cdc5d69e61f5e85bddd811c25530063b21
Stefan Matting <stefan.matting@gmail.com>
2019-12-13 19:13:56 +0100

    Set DB to postgres

--- insects_backend/settings.py
+++ insects_backend/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'insects_backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '195.201.97.57',
+        'USER': os.environ['ECO_USER'],
+        'PASSWORD': os.environ['ECO_PASSWORD'],
+        'NAME': 'eco'
     }
 }
 
